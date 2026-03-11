317702ba20751e3e2a3159362e32b6ce228ee856
Jagjeet Singh <99306360+singhjagjeet1@users.noreply.github.com>
2026-03-11 22:16:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function visitWebsite(){
+
+window.open("https://techBharat.com","_blank")
+
+}
+
+function scrollCourses(){
+
+document.getElementById("courses").scrollIntoView({
+behavior:"smooth"
+})
+
+}
