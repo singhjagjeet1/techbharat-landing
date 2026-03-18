6bdca75808a78109b8a9bb5dfe5882ea3be17354
Jagjeet Singh <99306360+singhjagjeet1@users.noreply.github.com>
2026-03-19 00:21:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 function visitWebsite(){
-
-window.open("https://techBharat.com","_blank")
-
-}
-
-function scrollCourses(){
-
-document.getElementById("courses").scrollIntoView({
-behavior:"smooth"
-})
-
+window.open("https://yourwebsite.com","_blank");
 }
