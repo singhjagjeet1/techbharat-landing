2a54fe433a0ad9d00393753413fab5122bfdc4dd
Jagjeet Singh <99306360+singhjagjeet1@users.noreply.github.com>
2026-03-19 23:18:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function visitWebsite(){
-window.open("https://yourwebsite.com","_blank");
+function openWhatsApp(){
+
+let phone = "919050358180"; // 🔥 replace with your number
+
+let message = "Hi, I am interested in TechBharat courses";
+
+let url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+window.open(url, "_blank");
+
 }
